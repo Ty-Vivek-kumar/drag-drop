be42f11a0eff10d4c82e2e1f7a7314a112482167
Ty-Vivek-kumar <76659217+Ty-Vivek-kumar@users.noreply.github.com>
2021-01-08 16:18:23 +0530

    Add files via upload

---
+++ drag.js
@@ -0,0 +1,53 @@
+console.log("******************************");
+const list_items = document.querySelectorAll('.list-item');
+const lists = document.querySelectorAll('.list');
+
+console.log("****************************");
+
+let draggedItem = null;
+
+for( let i = 0 ; i<list_items.length ; i++){
+    const item =list_items[i];
+   console.log(item);
+   item.addEventListener('dragstart',function(){
+       console.log('dragstart');
+       draggedItem = item;
+       setTimeout(function(){
+           item.style.display = 'none';
+       },0 );
+   });
+
+
+   item.addEventListener('dragend',function(){
+       setTimeout(function(){
+           draggedItem.style.display = 'block';
+           draggedItem = null ;
+
+       },0);
+   })
+
+   for(let j=0 ; j<lists.length ; j++){
+    const list= lists[j];
+
+
+    list.addEventListener('dragover', function(e){
+       e.preventDefault();
+    });
+    
+    list.addEventListener('dragenter', function(e){
+        e.preventDefault();
+     });
+
+     list.addEventListener('drop', function(e){
+         this.append(draggedItem);
+
+     })
+
+
+   }
+
+
+
+
+
+}
